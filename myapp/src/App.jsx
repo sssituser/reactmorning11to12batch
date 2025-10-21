3ead8f77ab2522979f5be9ea2dba6d4774127463
arun <allamarunkumar@gmail.com>
2025-10-21 12:18:52 +0530

    firstproject

---
+++ myapp/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+ 
+class App extends Component {
+  render() { 
+    return (
+      <React.Fragment>
+          <h1 className='text-success'>App Component</h1>
+          <button className='btn btn-lg btn-outline-primary '>ADD</button>
+      </React.Fragment>
+    );
+  }
+}
+ 
+export default App;
