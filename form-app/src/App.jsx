90a6e82e76e34a66810a9f06d3429075e17c5032
arun <allamarunkumar@gmail.com>
2025-11-03 12:12:52 +0530

    functionalcomponet wit form

--- form-app/src/App.jsx
+++ form-app/src/App.jsx
@@ -1,34 +1,13 @@
 import React, { useState } from "react";
+import Test from "./components/Test";
 export default function App(){
 
-  let[count,setCount]=useState(0);
-
-  let[msg,setMsg] = useState("Welcome To React")
-
-  let[employee,setEmployee]=useState({
-    employee:{
-      eid:111,
-      ename:"kiran",
-      esla:50000
-    }
-  })
 
   return(
     <React.Fragment>
-        <div className="container text-center">
-
-            <p className="h2">
-              Count : {count}
-            </p>
-
-            <p className="h2">
-              Message : {msg}
-            </p>
-            <p className="h2">
-              Employee :{JSON.stringify(employee)}
-            </p>
-
-        </div>
+      
+        <Test/>
+      
     </React.Fragment>
   )
 }
--- form-app/src/components/Test.jsx
+++ form-app/src/components/Test.jsx
@@ -1,10 +1,74 @@
-import React from "react";
+import React,{useState} from "react";
 
 export default function Test(){
+
+    let[employee,setEmployee]=useState(
+        {
+            
+                eid:111,
+                ename:"abc",
+                esal:50000,
+                dept:"Development"
+            
+        }
+    );
+
+    function updateInput(event){
+        setEmployee({
+            ...employee,
+            [event.target.name]:event.target.value
+        })        
+    }
+
 
     return(
         <React.Fragment>
             <p className="h4 text-center text-danger">Test Functional Component</p>
+            <p>{JSON.stringify(employee)}</p>
+        
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col-md-5">
+                        <div className="card">
+                            <div className="card-header bg-primary text-white">
+                                <p className="h1">Register Here</p>
+                            </div>
+                            <div className="card-body">
+                                <form action="">
+                                    <div className="form-group">
+                                    <input type="text" name='eid' value={employee.eid} 
+                                    placeholder="Employee id"
+                                    className="form-control"
+                                    onChange={updateInput}
+                                    />
+                                    </div>
+                                    <div className="form-group">
+                                    <input type="text" name='ename' value={employee.ename} 
+                                    placeholder="Employee Name"
+                                    className="form-control"
+                                    />
+                                    </div>
+                                    <div className="form-group">
+                                    <input type="text" name='esal' value={employee.esal} 
+                                    placeholder="Employee Salary"
+                                    className="form-control"
+                                    />
+                                    
+                                    </div>
+
+                                    <div className="form-group">
+                                    <input type="text" name='dept' value={employee.dept} 
+                                    placeholder="Department" className="form-control"/>
+                                    </div>
+                                    <button className="btn btn-sm btn-outline-primary">Register</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       
+       
         </React.Fragment>
     )
 }
