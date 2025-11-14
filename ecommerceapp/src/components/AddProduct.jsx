a76e1e60a0c3e50ac3f5f686a47b0779770502b8
arun <allamarunkumar@gmail.com>
2025-11-14 13:34:15 +0530

    ecommerceapp

--- ecommerceapp/src/components/AddProduct.jsx
+++ ecommerceapp/src/components/AddProduct.jsx
@@ -1,9 +1,75 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function AddProduct(){
+    let[product,setProduct]=useState({
+        pname:"",
+        pprice:"",
+        pqty:"",
+        pimage:""
+    })
+
+function updateInput(event){
+    setProduct(
+       {
+        ...product,
+       [event.target.name]:event.target.value
+       }
+    )
+}
+function save(){
+    axios.post("http://localhost:9000/products",product)
+    .then(()=>{
+        alert("Product Added")
+    }).catch((error)=>{alert(error)})
+}
+
     return(
         <React.Fragment>
-            <p className="h3 text-primary text-center">Add Product</p>
+           <div className="container">
+                <div className="row">
+                    <div className="col-md-5">
+                        <div className="card">
+                            <div className="card-header bg-primary text-white text-center">
+                                <p className="h2">Add Product</p>
+                            </div>
+                            <div className="card-body">
+                                <form action="" onSubmit={save}>
+                                    <div className="form-group">
+                                        <input type="text"
+                                         name="pname" 
+                                         value={product.pname}
+                                         onChange={updateInput}
+                                         placeholder="Product Name"  className="form-control"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text"
+                                         name="pprice"
+                                         onChange={updateInput}
+                                         value={product.pprice} placeholder="Product Price"  className="form-control"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="number" 
+                                        name="pqty" value={product.pqty}
+                                        onChange={updateInput}
+                                        placeholder="No of Products"  className="form-control"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="pimage" 
+                                        value={product.pimage}
+                                        onChange={updateInput}
+                                        placeholder="ImageLink"  className="form-control"/>
+                                    </div>
+                                    <button className="btn btn-sm btn-outline-primary">Add Product</button>
+                                    <Link to='/' className="btn btn-sm btn-outline-amber float-right">Back</Link>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+           </div>
         </React.Fragment>
     )
 }
--- ecommerceapp/src/components/ProductList.jsx
+++ ecommerceapp/src/components/ProductList.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
+import { Link } from "react-router-dom";
+
 export default function ProductList(){
 let[products,setProducts]=useState([]);
 useEffect(()=>{
@@ -14,6 +16,22 @@ useEffect(()=>{
 
     return(
         <React.Fragment>
+            <section>
+                <div className="container">
+                    <p className="lead">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo ipsa ullam placeat eligendi? Corporis nihil fugiat tempora distinctio eveniet debitis ullam officia repudiandae sit autem necessitatibus est, omnis delectus odit fugit eum! Cumque facilis temporibus, vitae et veritatis nam a iure laboriosam at enim dolor ad dolore soluta perferendis fugit harum distinctio, eum reiciendis doloribus saepe nulla provident iste? Tenetur?
+                    </p>
+                    
+                    <div className="row">
+                        <div className="col-md-8">
+                            <Link to='/add' className="btn btn-primary btn-sm">Add</Link>
+                        </div>
+                        <div className="col-md-4">
+                            <input type="text" className="form-control" placeholder="ID/Name/Price"/>
+                        </div>
+                    </div>
+                </div>
+            </section>
           <div className="container mt-5">
               {
                 products.length > 0 ?
@@ -27,6 +45,7 @@ useEffect(()=>{
                             <th>Price</th>
                             <th>Qty</th>
                             <th>Total</th>
+                            <th>Operations</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -40,6 +59,11 @@ useEffect(()=>{
                                         <td>{prod.pprice}</td>
                                         <td>{prod.pqty}</td>
                                         <td>{prod.pqty*prod.pprice}</td>
+                                        <td>
+                                            <i className="fa fa-eye text-primary mr-3" />
+                                            <i className="fa fa-pen text-warning mr-3"/>
+                                            <i className="fa fa-trash text-danger mr-3"/>
+                                        </td>
                                     </tr>
                                 )
                             })
